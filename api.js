e3835b11fe67838b35052d9ac9587f5ae179e896
thechieftech <chris@thechieftech.com>
2018-03-06 15:24:52 +0200

    removed appConfig ref from start and corsOptions

--- api.js
+++ api.js
@@ -2,16 +2,11 @@ const express = require('express');
 const bodyParse = require('body-parser');
 require('./dal/database');
 const config = require('config');
-var appConfig = config.get('appConfig');
 
 const app = express();
 
 app.use(bodyParse.urlencoded({extended: true}))
 .use(bodyParse.json());
-
-var corsOptions = {
-  origin: appConfig.corsOrigin
-}
 
 app.use('/api', require('./routes/product'));
 app.use('/api', require('./routes/category'));
